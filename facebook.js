4df68bb7df62793e8cdd1f42dd525ca8931bf826
Zack Grannan <zgrannan@gmail.com>
2014-05-17 12:32:27 -0700

    Started FB Driver

---
+++ facebook.js
@@ -0,0 +1,18 @@
+var FB   = require('fb');
+var util = require('./util');
+
+function getGroupPosts(groupId, accessToken, callback, limit){
+  if (typeof limit === 'undefined') limit = 20;
+
+  if (limit > 200)  throw Exception("Count cannot be larger than 200");
+  if (limit < 0)    throw Exception("Count cannot be less than 0");
+  if (limit === 0)  return callback({data: []});
+
+  var post_fields = ['id', 'from', 'message', 'created_time', 'updated_time'];
+  FB.setAccessToken(accessToken);
+  FB.api("/" + groupId + '/feed/', {'fields': post_fields, 'limit': limit}, callback);
+}
+
+module.exports = {
+  'getGroupPosts': getGroupPosts
+};
