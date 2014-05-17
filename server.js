e53102d09b19951306fefd851f207a674ad5c7f9
Zack Grannan <zgrannan@gmail.com>
2014-05-17 12:30:00 -0700

    Moved logic to `server.js` for easier testing

---
+++ server.js
@@ -0,0 +1,21 @@
+var util     = require('./util');
+var mustache = require('mustache-express');
+var express  = require('express');
+var passport = require('passport');
+var app  = express();
+
+app.set('view engine', 'mustache');
+app.engine('html', mustache());
+
+app.configure(function(){
+  app.use(express.cookieParser());
+  app.use(express.session({secret: util.getVar('SESSION_SECRET')}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+});
+
+
+routes = require('./routes');
+routes(app);
+
+module.exports = {"app": app};
