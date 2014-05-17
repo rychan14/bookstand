43473576548503796c5bfc49b9936881b459a503
Zack Grannan <zgrannan@gmail.com>
2014-05-17 12:33:27 -0700

    Homepage now shows access token for debugging

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,5 @@ exports.index = function(req, res){
   if (!req.user)
     return res.render('index.html');
   else
-    return res.render('home.html');
+    return res.render('home.html', {'access_token': req.user.token});
 };
